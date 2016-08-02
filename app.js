c16efd7063031ca5556a4a7f08f8e246f3e3e6ea
akshitbrightlifecare <akshit.sawhney@brightlifecare.com>
2016-08-02 17:53:39 +0530

    JSON Related changes
    JSON Related changes

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-console.log("inside");
-$.get("data.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
+var obj = jQuery.parseJSON( '[{ "name": "John" },{"name": "Akshit"}]' );
+var cList = $("#myID");
+$.each(obj, function(i)
+{
+    console.log(obj[i].name);
+    var li = $('<li/>')
+        .addClass('ui-menu-item')
+        .attr('role', 'menuitem')
+        .text(obj[i].name)
+        .appendTo(cList);
 });
